951da88df92aa86e366a82ef7fd88fcf9919412b
yuuki1293 <yuuki1293@outlook.jp>
2023-06-20 22:22:01 +0900

    定数をdefine

--- src/global.cpp
+++ src/global.cpp
@@ -5,11 +5,11 @@ body w;
 int32_t *stack;
 body **rstack;
 
-int32_t stack_start[256];
-body *rstack_start[64];
-char input_buf[64];
-int32_t user_mem[256];
-word dict_mem[64];
+int32_t stack_start[FORTH_STACK_SIZE];
+body *rstack_start[FORTH_RSTACK_SIZE];
+char input_buf[FORTH_INPUT_BUF];
+int32_t user_mem[FORTH_USER_MEM];
+word dict_mem[FORTH_DICT_MEM];
 
 uint32_t state = 0;
 word *here = dict_mem;
--- src/global.hpp
+++ src/global.hpp
@@ -1,19 +1,26 @@
 #ifndef GLOBAL_HPP
 #define GLOBAL_HPP
 
+#include <stdio.h>
 #include <stdint.h>
 #include "forth_def.hpp"
+
+#define FORTH_STACK_SIZE 256
+#define FORTH_RSTACK_SIZE 64
+#define FORTH_INPUT_BUF 256
+#define FORTH_USER_MEM 256
+#define FORTH_DICT_MEM 64
 
 extern body *pc;
 extern body w;
 extern int32_t *stack;
 extern body **rstack;
 
-extern int32_t stack_start[256];
-extern body *rstack_start[64];
-extern char input_buf[64];
-extern int32_t user_mem[256];
-extern word dict_mem[64];
+extern int32_t stack_start[FORTH_STACK_SIZE];
+extern body *rstack_start[FORTH_RSTACK_SIZE];
+extern char input_buf[FORTH_INPUT_BUF];
+extern int32_t user_mem[FORTH_USER_MEM];
+extern word dict_mem[FORTH_DICT_MEM];
 
 extern uint32_t state;
 extern word *here;
