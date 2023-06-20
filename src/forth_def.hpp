984900a33df611b642f3d3b81056925bcbb1b684
yuuki1293 <yuuki1293@outlook.jp>
2023-06-20 15:58:22 +0900

    bodyをwordの前に置く

--- src/forth_def.hpp
+++ src/forth_def.hpp
@@ -1,18 +1,18 @@
 #ifndef FORTH_DEF_HPP
 #define FORTH_DEF_HPP
 
+union body
+{
+    void (*impl)();
+    body *xt;
+};
+
 struct word
 {
     struct word *next;
     char *name;
     bool flag;
     body xt;
-};
-
-union body
-{
-    void (*impl)();
-    body *xt;
 };
 
 #endif // FORTH_DEF_HPP
