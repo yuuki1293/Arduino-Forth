4324ffccb5caf4d9df3a4280cd44e315f7360c04
yuuki1293 <yuuki1293@outlook.jp>
2023-06-20 12:14:05 +0900

    変数をextren

--- src/global.hpp
+++ src/global.hpp
@@ -13,4 +13,8 @@ extern char input_buf[256];
 extern int32_t user_mem[1024];
 extern word dict_mem[256];
 
+extern void* stack_base;
+extern uint8_t state;
+extern void* here;
+
 #endif //GLOBAL_HPP
