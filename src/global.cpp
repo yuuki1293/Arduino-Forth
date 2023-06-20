8d2b1d89c182b22157b0284ef50f62e6d9cba69c
yuuki1293 <yuuki1293@outlook.jp>
2023-06-20 13:19:20 +0900

    rstackの型を変更

--- src/global.cpp
+++ src/global.cpp
@@ -3,10 +3,10 @@
 void (**pc)();
 void (*w)();
 int32_t *stack;
-void (**rstack)();
+void (***rstack)();
 
 int32_t stack_start[1024];
-void (*rstack_start[64])();
+void (**rstack_start[64])();
 char input_buf[256];
 int32_t user_mem[1024];
 word dict_mem[256];
--- src/global.hpp
+++ src/global.hpp
@@ -7,10 +7,10 @@
 extern void (**pc)();
 extern void (*w)();
 extern int32_t* stack;
-void (**rstack)();
+void (***rstack)();
 
 extern int32_t stack_start[1024];
-extern void (*rstack_start[64])();
+extern void (**rstack_start[64])();
 extern char input_buf[256];
 extern int32_t user_mem[1024];
 extern word dict_mem[256];
