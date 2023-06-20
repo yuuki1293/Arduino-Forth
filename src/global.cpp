b5db741a5cf37c0959bf9588e80cf590bcfad569
yuuki1293 <yuuki1293@outlook.jp>
2023-06-20 13:06:42 +0900

    rstackを定義

--- src/global.cpp
+++ src/global.cpp
@@ -2,12 +2,14 @@
 
 void (**pc)();
 void (*w)();
-int32_t* stack;
+int32_t *stack;
+void (**rstack)();
 
 int32_t stack_start[1024];
+void (*rstack_start[64])();
 char input_buf[256];
 int32_t user_mem[1024];
 word dict_mem[256];
 
 uint32_t state = 0;
-word* here = dict_mem;
+word *here = dict_mem;
