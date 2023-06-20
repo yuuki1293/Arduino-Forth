6e97fa2902f7eb2e9de20a8c08ffd0afc1685061
yuuki1293 <yuuki1293@outlook.jp>
2023-06-20 12:06:13 +0900

    swapを実装

--- src/words.cpp
+++ src/words.cpp
@@ -21,4 +21,14 @@ native1(drop)
 }
 #define _lw ref(drop)
 
+native1(swap)
+{
+    int32_t a, b;
+    a = pop();
+    b = pop();
+    push(a);
+    push(b);
+    next();
+}
+
 word *last_word = _lw;
