b16fa38bc237840532374770090b56ac29572268
yuuki1293 <yuuki1293@outlook.jp>
2023-06-20 11:37:24 +0900

    矛盾を解消

--- src/global.cpp
+++ src/global.cpp
@@ -5,6 +5,6 @@ void (*w)();
 int32_t* stack;
 
 int32_t stack_start[1024];
-char input_buf[1024];
-int32_t user_mem[65536];
-word dict_mem[65536];
+char input_buf[256];
+int32_t user_mem[1024];
+word dict_mem[256];
