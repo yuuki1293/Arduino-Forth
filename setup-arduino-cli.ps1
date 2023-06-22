2bf167750c12b9af8e9b2e689c56e787867fda8b
yuuki1293 <yuuki1293@outlook.jp>
2023-06-22 22:46:48 +0900

    setup-arduino-cli.ps1を作成

---
+++ setup-arduino-cli.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest -Uri "https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Windows_64bit.zip" -OutFile "bin/arduino-cli.zip"
+Expand-Archive -Path bin\arduino-cli.zip -DestinationPath bin\
