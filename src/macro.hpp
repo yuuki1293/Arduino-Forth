0d8b7471d34793a1b0e202cb3d9af51659f2a341
yuuki1293 <yuuki1293@outlook.jp>
2023-06-25 15:24:51 +0900

    constつけるべきグローバル変数にconstをつける

--- src/macro.hpp
+++ src/macro.hpp
@@ -18,42 +18,42 @@
         .value = a \
     }
 
-#define native3(name, label, flag)              \
-    [[noreturn]] void i_##label();              \
-    body a_##label[] = {{.impl_p = i_##label}}; \
-    forth_word w_##label = {_lw,                \
-                            name,               \
-                            flag,               \
-                            a_##label};         \
+#define native3(name, label, flag)                    \
+    [[noreturn]] void i_##label();                    \
+    const body a_##label[] = {{.impl_p = i_##label}}; \
+    const forth_word w_##label = {_lw,                \
+                                  name,               \
+                                  flag,               \
+                                  a_##label};         \
     [[noreturn]] void i_##label()
 
 #define native2(name, label) native3(name, label, 0)
 
 #define native1(label) native2(#label, label)
 
-#define colon3(name, label, flag)       \
-    extern body a_##label[];            \
-    forth_word w_##label = {_lw,        \
-                            name,       \
-                            flag,       \
-                            a_##label}; \
-    body a_##label[] =
+#define colon3(name, label, flag)             \
+    extern const body a_##label[];            \
+    const forth_word w_##label = {_lw,        \
+                                  name,       \
+                                  flag,       \
+                                  a_##label}; \
+    const body a_##label[] =
 
 #define colon2(name, label) colon3(name, label, 0)
 
 #define colon1(label) colon2(#label, label)
 
-#define const2(label, var)                      \
-    [[noreturn]] void i_##label()               \
-    {                                           \
-        push((intptr_t)var);                    \
-        next();                                 \
-    }                                           \
-    body a_##label[] = {{.impl_p = i_##label}}; \
-    forth_word w_##label = {_lw,                \
-                            #label,             \
-                            0,                  \
-                            a_##label}
+#define const2(label, var)                            \
+    [[noreturn]] void i_##label()                     \
+    {                                                 \
+        push((intptr_t)var);                          \
+        next();                                       \
+    }                                                 \
+    const body a_##label[] = {{.impl_p = i_##label}}; \
+    const forth_word w_##label = {_lw,                \
+                                  #label,             \
+                                  0,                  \
+                                  a_##label}
 
 #define const1(label) const2(label, label)
 
